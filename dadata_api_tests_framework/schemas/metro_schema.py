61278d6b59bf044579925fb4adb41a1d55fff459
maria.m <maryqatest9@gmail.com>
2024-01-17 23:22:07 +0300

    fixes for test_find_metro_station

--- dadata_api_tests_framework/schemas/metro_schema.py
+++ dadata_api_tests_framework/schemas/metro_schema.py
@@ -1,76 +1,76 @@
 METRO_SCHEMA = {
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "type": "object",
-  "properties": {
-    "suggestions": {
-      "type": "array",
-      "items": [
-        {
-          "type": "object",
-          "properties": {
-            "value": {
-              "type": "string"
-            },
-            "unrestricted_value": {
-              "type": "string"
-            },
-            "data": {
-              "type": "object",
-              "properties": {
-                "city_kladr_id": {
-                  "type": "string"
-                },
-                "city_fias_id": {
-                  "type": "string"
-                },
-                "city": {
-                  "type": "string"
-                },
-                "name": {
-                  "type": "string"
-                },
-                "line_id": {
-                  "type": "string"
-                },
-                "line_name": {
-                  "type": "string"
-                },
-                "geo_lat": {
-                  "type": "number"
-                },
-                "geo_lon": {
-                  "type": "number"
-                },
-                "color": {
-                  "type": "string"
-                },
-                "is_closed": {
-                  "type": "boolean"
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+        "suggestions": {
+            "type": "array",
+            "items": [
+                {
+                    "type": "object",
+                    "properties": {
+                        "value": {
+                            "type": "string"
+                        },
+                        "unrestricted_value": {
+                            "type": "string"
+                        },
+                        "data": {
+                            "type": "object",
+                            "properties": {
+                                "city_kladr_id": {
+                                    "type": "string"
+                                },
+                                "city_fias_id": {
+                                    "type": "string"
+                                },
+                                "city": {
+                                    "type": "string"
+                                },
+                                "name": {
+                                    "type": "string"
+                                },
+                                "line_id": {
+                                    "type": "string"
+                                },
+                                "line_name": {
+                                    "type": "string"
+                                },
+                                "geo_lat": {
+                                    "type": "number"
+                                },
+                                "geo_lon": {
+                                    "type": "number"
+                                },
+                                "color": {
+                                    "type": "string"
+                                },
+                                "is_closed": {
+                                    "oneOf": [
+                                        {"type": "boolean"},
+                                        {"type": "null"}]}},
+                            "required": [
+                                "city_kladr_id",
+                                "city_fias_id",
+                                "city",
+                                "name",
+                                "line_id",
+                                "line_name",
+                                "geo_lat",
+                                "geo_lon",
+                                "color"
+                            ]
+                        }
+                    },
+                    "required": [
+                        "value",
+                        "unrestricted_value",
+                        "data"
+                    ]
                 }
-              },
-              "required": [
-                "city_kladr_id",
-                "city_fias_id",
-                "city",
-                "name",
-                "line_id",
-                "line_name",
-                "geo_lat",
-                "geo_lon",
-                "color"
-              ]
-            }
-          },
-          "required": [
-            "value",
-            "unrestricted_value",
-            "data"
-          ]
+            ]
         }
-      ]
-    }
-  },
-  "required": [
-    "suggestions"
-  ]
+    },
+    "required": [
+        "suggestions"
+    ]
 }
